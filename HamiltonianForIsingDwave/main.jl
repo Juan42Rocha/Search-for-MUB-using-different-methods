0f8f207dfafbd3d611c4669922d268035b9ed038
Hans Baltazar <85709443+Juan42Rocha@users.noreply.github.com>
2024-04-22 10:37:56 -0600

    Add files via upload

--- HamiltonianForIsingDwave/main.jl
+++ HamiltonianForIsingDwave/main.jl
@@ -14,14 +14,68 @@ include("fun.jl")
 
 
 function main()
-    n = 2;
-    k = 2;
+    d = 2; n = 1; k = 2;
+    Hu =  oneUpforVec(d,n,k,0,1);
+    Ho  =  HOrth(d,n,k,0,1);
+    Hi  = Hising(d,n,k,2);
+    if n>1 
+        Hm = HMubs(d,n,k,0,1)
+    end
     
-
-
-
-
-
+  # H = [-1  1  1 1 ;
+  #      0  -1 1  1;
+  #      0  0  -1 1;
+  #      0  0  0  -1];
+  # E = Energia(H);
   
+
+end
+
+function Nminst()
+    d = 2 ; n = 2; k = 2;
+    Ho = HOrth(d,n,k,0,1);
+    Hi = Hising(d,n,k,2);
+    Hu = oneUpforVec(d,n,k,0,1);
+    
+    B = collect(-7:0.01:7);
+    of = open("HoHu-B-d"*string(2)*"n"*string(n)*"k"*string(k)*".dat","w");
+   
+    for b in B;
+        Em,t = findM(Ho+Hu,b);
+        nt = size(t,1);
+        mt = mean(sum.(VecConf.(t.-1,n*d*d^k)));
+        @printf(of,"%f    %f   %f   %f\n",b,nt,Em,mt);
+        println(b,"" ,nt," ",Em," " ,mt)
+    end
+   
+    close(of);
+end 
+
+
+
+
+
+function saveH()
+    d = 2 ; n = 2; k = 4;
+    Ho = HOrth(d,n,k,0,1);
+    Hi = Hising(d,n,k,1);
+    Hu = oneUpforVec(d,n,k,0,1);
+    if n>1 
+        Hm = HMubs(d,n,k,0,1)
+    end
+
+    # Horht= Ortogonalidad(d,k,0,1);
+    # Hmub = Mubsness(d,k,0,1);
+    #  open("Horht"*string(d)*"n"*string(n-1)*"k"*string(k)*".dat","w") do io
+    #     writedlm(io,Horht)
+    # end
+    # open("Hmubs"*string(d)*"n"*string(n-1)*"k"*string(k)*".dat","w") do io
+    #     writedlm(io,Hmub)
+    # end  
+ 
+   
+    open("HoHm"*string(d)*"n"*string(n)*"k"*string(k)*".dat","w") do io
+        writedlm(io,Ho+Hm)
+    end
 
 end
